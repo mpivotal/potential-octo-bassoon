9f4fa693e09e60ce5ca463de455f35778d9cde07
Mike Long <mlong@pivotal.io>
2016-04-28 10:57:51 -0700

    rendering css

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require( 'express')
 var app = express()
+var lessMiddleware = require('less-middleware');
 
 app.set( 'views', __dirname + '/views')
 app.set( 'view engine', 'jade')
+app.use(lessMiddleware(__dirname + '/less', { dest: 'public/styles/' }));
 app.use( express.static( __dirname + '/public'))
+app.locals.pretty = true
 
 app.get( '/', function ( req, res) {
-  res.render( 'pages/index', { })
+  res.render( 'pages/org', { })
 })
 
 app.listen( process.env.PORT || 4000)
