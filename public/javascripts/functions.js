4eaf363f895ec8547187507667eabd374ef59174
Mike Long <mlong@pivotal.io>
2016-05-05 11:35:06 -0700

    enable app actions

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,3 +1,18 @@
 $(document).ready( function() {
+  if ( $('.org .apps').length != 0 ) {
+    // app selection
+    var checkboxes = $("input[type='checkbox'][name='apps[]']")
+    var actions = $("button#app-actions")
+    $("input[type='checkbox'][name='apps[]'][value='all']").click( function() {
+      checkboxes.prop('checked', checkboxes.prop('checked'))
+    })
+    checkboxes.click( function() {
+      if ($("input:checkbox:checked").length > 0) {
+        actions.prop('disabled', false)
+      } else {
+        actions.prop('disabled', true)
+      }
+    })
+  }
   console.log( 'hi' )
 })
